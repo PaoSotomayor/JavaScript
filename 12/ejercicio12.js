30a1aa56117b865ef6d8e1d8db0d4ab0413eac8e
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-16 20:23:08 -0500

    ejercicio 12 corregido

--- 12/ejercicio12.js
+++ 12/ejercicio12.js
@@ -1,4 +1,18 @@
 // Escribir una función flecha de JavaScript que reciba un argumento y retorne el tipo de dato
 
-let data = () => console.log(typeof 'soy un String');
-data();
+let TipoDato = (dato) => {
+    return typeof dato;
+};
+
+let input = prompt("Ingresa un dato");
+let dato;
+
+if (!isNaN(input)) {
+    dato = parseFloat(input); //Convierto el dato en un número
+} else if (input.toLowerCase() === "true" || input.toLowerCase() === "false") {
+    dato = input.toLowerCase() === "true"; //Convertirlo en un boolean
+} else {
+    dato = input;
+}
+
+alert("El tipo de dato es: " + TipoDato(dato));
---
+++ 17/ejercicio17.js
@@ -0,0 +1,8 @@
+// Realizar un programa que elimine los dos últimos elementos de un array. Mostrar el resultado
+
+var miArray = [10, 20, 30, 40, 50, 60];
+
+miArray.splice(-2);
+
+// Mostrar el arreglo resultante
+console.log("Arreglo después de eliminar los dos últimos elementos:", miArray);
