d8445500aa487d897334a0498da44f3240404afe
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-17 21:51:44 -0500

    corregido el 14

--- 14/ejercicio14.js
+++ 14/ejercicio14.js
@@ -2,7 +2,9 @@
 // Número de páginas. Crear un método para cargar un libro pidiendo los datos al usuario y luego informar 
 // mediante otro método el número de ISBN, el título, el autor del libro y el numero de páginas.
 
-prompt('Ingrese los datos del libro' + cargarLibro() + mostrarLibro())
+alert('Ingrese los datos del libro')
+
+mostrarLibro()
 
 const libro = {
     ISBN: 0,
