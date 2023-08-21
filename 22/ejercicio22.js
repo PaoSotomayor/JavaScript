ae2c9f662728653e1a2b0343382aac1ff12dc56e
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-21 18:12:02 -0500

    Realizado hasta el 23

--- 22/ejercicio22.js
+++ 22/ejercicio22.js
@@ -1,1 +1,6 @@
 // Escribir un programa de JavaScript que al clickear un botón muestre un mensaje a elección.
+
+const boton = document.getElementById('boton');
+boton.addEventListener('click', function () {
+    alert('¡Soy un botón bien feo!');
+});
