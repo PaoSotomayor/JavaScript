b08fbfd8ed2d0b414d1e7368ca0d094f5da4ef8d
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-22 21:34:40 -0500

    14 con stringify

--- 14/ejercicio14.js
+++ 14/ejercicio14.js
@@ -1,28 +1,19 @@
 // Crear un objeto libro que contenga las siguientes propiedades: ISBN, Título, Autor, 
 // Número de páginas. Crear un método para cargar un libro pidiendo los datos al usuario y luego informar 
 // mediante otro método el número de ISBN, el título, el autor del libro y el numero de páginas.
-
-alert('Ingrese los datos del libro' + cargarLibro())
-mostrarLibro();
-
-const Libro = {
-    ISBN: 0,
-    Titulo: ' ',
-    Autor: ' ',
-    NumPag: 0,
-}
-var libro1 = new Libro();
-function cargarLibro() {
-    ISBN = prompt('Ingrese el número de ISBN');
-    Titulo = prompt('Ingrese el título');
-    Autor = prompt('Ingrese el Autor');
-    NumPag = prompt('Ingrese el Número de Páginas');
+let libro = { isbn: 0, titulo: "", autor: "", numPage: 0 };
+//let libroM={};
+function crearLibro() {
+    libro.isbn = prompt("Ingrese el isbn del libro:");
+    libro.titulo = prompt("Ingrese el titulo del autor:");
+    libro.autor = prompt("Ingrese el autor del libro:");
+    libro.numPage = prompt("Ingrese el número de páginas del libro:");
 }
 
 function mostrarLibro() {
-    alert(`ISBN: ${ISBN} 
-   Título:  ${Titulo} 
-   Autor:  ${Autor} 
-   Número de páginas: ${NumPag}`)
+
+    alert(JSON.stringify(libro));
 }
 
+crearLibro();
+mostrarLibro();
