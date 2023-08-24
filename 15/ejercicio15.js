cd7b8889df849873fc29adaf801b66cdcfdfa20d
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-24 15:26:27 -0500

    15 con JSON.stringify

--- 15/ejercicio15.js
+++ 15/ejercicio15.js
@@ -1,23 +1,25 @@
 // Escribe un programa JavaScript para calcular el área y el perímetro de un objeto Círculo con la propiedad radio.
 // Nota: Cree dos métodos para calcular el área y el perímetro. El radio del círculo lo proporcionará el usuario.
 
-class Circulo {
-    constructor(radio) {
-        this.radio = radio;
-    }
-
-    calcularArea = () => Math.PI * this.radio * this.radio;
-    calcularPerimetro = () => 2 * Math.PI * this.radio;
+let circulo = {
+    radio: 0,
+    area: 0.00,
+    perimetro: 0.00
 }
 
-let radio = Number(prompt("Ingrese el radio del círculo: "));
+ingresarRadio = () => {
+    circulo.radio = prompt('Indique el radio del círculo')
+}
+calcularArea = () => circulo.area = Math.PI * Math.pow(circulo.radio, 2);
+calcularPerimetro = () => circulo.perimetro = 2 * Math.PI * circulo.radio;
 
-// Crear una instancia de la clase Circulo
-let circulo = new Circulo(radio);
 
-// Calcular el área y el perímetro utilizando los métodos
-let area = circulo.calcularArea();
-let perimetro = circulo.calcularPerimetro();
+ingresarRadio();
+calcularArea();
+calcularPerimetro();
+alert(JSON.stringify(circulo));
 
-alert("El área del círculo es: " + area.toFixed(2) +
-    " y el perímetro  es: " + perimetro.toFixed(2));
+// alert("El área del círculo es: " + circulo.area.toFixed(2) +
+//     " y el perímetro  es: " + circulo.perimetro.toFixed(2));
+
+
