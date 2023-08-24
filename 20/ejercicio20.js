26b7466ea5a55442e8929e732a3e985760c411f4
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-24 15:37:48 -0500

    ..

--- 20/ejercicio20.js
+++ 20/ejercicio20.js
@@ -1,18 +1,18 @@
 // Realizar un programa que obtenga la siguiente matriz [[3], [6], [9], [12], [15]] y devuelve y
 // muestre el siguiente array[6, 9, 12, 15, 18].
 
-let matriz = [[3], [6], [9], [12], [15]];
-let array = [];
-for (let i = 0; i < matriz.length; i++) {
-    array.push(matriz[i][0] + 3);
-}
-alert('[' + array + ']');
+// let matriz = [[3], [6], [9], [12], [15]];
+// let array = [];
+// for (let i = 0; i < matriz.length; i++) {
+//     array.push(matriz[i][0] + 3);
+// }
+// alert('[' + array + ']');
 
 //otra forma
-// let array = [[3], [6], [9], [12], [15]];
+let array = [[3], [6], [9], [12], [15]];
 
-// array = array.flat();
-// array.splice(0,2);
-// array.push(18);
+array = array.flat();
+array.splice(0, 1);
+array.push(18);
 
-// console.log(array);
+console.log(array);
